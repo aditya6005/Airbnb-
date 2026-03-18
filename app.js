ff343b41eb60988d2f51c762a3c7ebc39542de3d
aditya6005 <ak0270528@gmail.com>
2026-03-18 10:18:24 +0530

    Intial Setup Complete

---
+++ app.js
@@ -0,0 +1,121 @@
+// step 1 -------------------
+const express= require("express");
+const app=express();
+const mongoose= require("mongoose");
+// step 4---------
+const mongo_url = "mongodb://127.0.0.1:27017/wanderlust";
+// // step 11---------
+// const Listing = require("./models/listing.js");
+
+// step 15 ----------------
+const path=require("path");
+const methodOverride=require("method-override");
+const ejsMate= require("ejs-mate");
+const ExpressError=require("./utils/ExpressError.js");
+const session = require("express-session");
+const listingsRouter = require("./routes/listing.js");
+const reviewsRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
+  
+
+// step 6------------------
+main()
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+// step 5 -----------
+async function main(){
+    await mongoose.connect(mongo_url);
+}
+
+// step 16----------------
+app.set("view engine", "ejs");
+app.set("views",path.join(__dirname,"views"));
+
+// step 18-------------------------
+app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"))
+app.engine("ejs",ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
+
+const sessionOptions= {
+  secret: "mysupersecretcode",
+  resave:false,
+  saveUninitialized:true,
+  cookie:{
+    expires:Date.now() +7*24*60*60*1000,
+    maxAge:7*24*60*60*1000,
+    httpOnly:true,
+  },
+};
+  
+// step 3----------------
+app.get("/",(req,res)=>{
+    res.send("hi , i am root ");
+});
+
+
+app.use(session(sessionOptions));
+app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.use((req,res,next)=>{
+  res.locals.success= req.flash("success");
+  res.locals.error= req.flash("error");
+  res.locals.currUser= req.user;
+  next();
+});
+
+// app.get("/demouser", async(req,res)=>{
+//   let fakeUser = new User({
+//     email:"student@gmail.com",
+//     username:"delta-student",
+//   });
+//   let registeredUser =await User.register(fakeUser, "helloworld");
+//   res.send(registeredUser);
+ 
+// });
+
+
+// step 14------------------
+app.use("/listings" , listingsRouter);
+app.use("/listings/:id/reviews", reviewsRouter);
+app.use("/", userRouter);
+
+
+
+app.use((err,req,res,next)=>{
+    let{statusCode=500,message="Something went WRONG !"}=err;
+    res.status(statusCode).render("listings/error.ejs",{message});
+    // res.status(statusCode).send(message);
+});
+
+// step -2
+app.listen(8080,()=>{
+    console.log("server is listening to port 8080");
+});
+
+
+
+
+// app.use(flash());
+
+// app.use((req,res,next)=>{
+//     res.locals.error = req.flash("error");
+//     res.locals.success = req.flash("success");
+//     next();
+// });
---
+++ routes/listing.js
@@ -0,0 +1,97 @@
+const express = require("express");
+const router = express.Router();
+const wrapAsync= require("../utils/wrapAsync.js");
+
+const Listing = require("../models/listing.js");
+const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
+
+
+
+
+// index route
+router.get("/",  wrapAsync(async (req,res)=>{
+const allListings =await  Listing.find({});
+res.render("../views/listings/index.ejs",{allListings});
+}));
+
+
+// STEP 19-----------------------------
+// new route
+router.get("/new",isLoggedIn, (req,res)=>{
+    
+res.render("listings/new.ejs")
+});
+
+// step 17--------------------
+// show route
+router.get("/:id", wrapAsync(async (req, res)=>{
+    let {id} =req.params;
+    const listing= await Listing.findById(id)
+    .populate({
+        path:"reviews",
+        populate:{
+            path:"author",
+        },
+    }).populate("owner");
+    if(!listing){
+        req.flash("error", "Listing you requested for does not exist !");
+        res.redirect("/listings");
+    }
+    res.render("../views/listings/show.ejs", {listing});
+}));
+
+
+// step 20------------------
+// create route
+router.post("/",validateListing,isLoggedIn,wrapAsync(async (req,res,next) =>{
+        const newListing = new Listing(req.body.listing); 
+        newListing.owner = req.user._id;
+        
+        await newListing.save();
+        req.flash("success", "New Listing Created");
+        res.redirect("/listings");
+}));
+
+// Edit route
+
+router.get("/:id/edit",isLoggedIn,
+    isOwner,
+    wrapAsync(async (req,res)=>{
+    let {id} = req.params;
+    const listing= await Listing.findById(id);
+    if(!listing){
+        req.flash("error", "Listing you requested for does not exist !");
+        res.redirect("/listings");
+        
+    }
+    res.render("listings/edit.ejs", {listing});
+
+}));
+
+// update route
+router.put("/:id",
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(async (req,res)=>{
+    let {id}= req.params;
+    await  Listing.findByIdAndUpdate(id, {...req.body.listing});
+    req.flash("success", "Listing Updated !");
+    res.redirect(`/listings/${id}`);
+}));
+
+// DELETE ROUTE
+router.delete("/:id",isLoggedIn,
+    isOwner,
+    wrapAsync(async (req,res) =>{
+    let {id} =req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    req.flash("success", "Listing deleted");
+
+    res.redirect("/listings");
+}));
+
+
+module.exports=router;
+
